9a4c0852a26dae0a14b53b4a6515463253c5281b
Daniele Andreetta <danieleandree91@gmail.com>
2024-01-12 16:04:50 +0100

    commit

---
+++ src/assets/script/createCard.js
@@ -0,0 +1,32 @@
+export function createNewsCard(container, obj, defaultImageUrl) {
+  const div = createElement("div", "news-card", container);
+  div.addEventListener("click", () => {
+    window.location.href = obj.url;
+  });
+  const imgDiv = createElement("div", "news-card-img", div);
+  imgDiv.style.backgroundImage = `url(${defaultImageUrl})`;
+  imgDiv.style.backgroundImage = `url(${obj.imageUrl})`;
+  imgDiv.style.backgroundSize = "cover";
+  const footerDiv = createElement("div", "news-card-footer", div);
+  const newsTitle = createElement("h2", "news-title", footerDiv, obj.title);
+  const dataAndSite = createElement("div", "data-and-site", footerDiv);
+  const linkToSite = createElement("a", "news-link", dataAndSite, getDomainUrl(obj.url));
+  linkToSite.href = "http://www." + getDomainUrl(obj.url);
+  const newsData = createElement("p", "news-data", dataAndSite, getData(obj.time));
+}
+
+function createElement(tag, className, container, text = null) {
+  const element = document.createElement(tag);
+  element.className = className;
+  container.appendChild(element);
+  text ? (element.innerHTML = text) : null;
+  return element;
+}
+
+function getData(tempo) {
+  return new Date(tempo * 1000).toLocaleString().slice(0, -3);
+}
+
+function getDomainUrl(url) {
+  return url.replace("http://", "").replace("https://", "").replace("www.", "").split(/[/?#]/)[0];
+}
---
+++ src/assets/script/main.js
@@ -0,0 +1,85 @@
+import "../css/style.css";
+import defaultnewsImage from "../img/newsDefault.png";
+// import favicon from "../img/hacker-news-icon.png";
+import _ from "lodash";
+import { createNewsCard } from "./createCard.js";
+
+class NewsApp {
+  container = document.getElementById("news-container");
+  button = document.getElementById("pulsanteLoadMore");
+  baseUrl = "https://hacker-news.firebaseio.com/v0/";
+  idUrl = new URL(this.baseUrl + "newstories.json");
+  proxyUrl = "https://corsproxy.io/?";
+  idArray = [];
+  defaultImageUrl = defaultnewsImage;
+  testoPulsanteErrore = "Errore... Clicca per ricaricare";
+  constructor() {
+    this.button.addEventListener("click", this.getNewsFromId.bind(this));
+  }
+
+  async getNewsIds() {
+    try {
+      let response = await fetch(this.idUrl);
+      this.idArray = await response.json();
+      return 1;
+    } catch (e) {
+      console.log("getNewsIds Error: " + e);
+      this.idArray = [];
+      this.button.innerHTML = this.testoPulsanteErrore;
+      return 0;
+    }
+  }
+
+  async getNewsFromId() {
+    for (let i = 0; i < 10; i++) {
+      let id = this.idArray.shift();
+      if (!id) {
+        newsTest = await this.getNewsIds();
+        if (newsTest == 0) {
+          break;
+        }
+        i--;
+        continue;
+      }
+      this.button.innerHTML = "Caricamento... [" + i + "/10]";
+      try {
+        let newsUrl = new URL(this.baseUrl + "item/" + id + ".json");
+        let response = await fetch(newsUrl);
+        var data = await response.json();
+        let dataUrl = _.get(data, "url", 0);
+        let dataTitle = _.get(data, "title", 0);
+        if (!data || dataTitle == 0 || dataUrl == 0) {
+          i--;
+          continue;
+        }
+      } catch (e) {
+        console.log("getNewsFromId Error: " + e);
+        i--;
+        continue;
+      }
+      data.imageUrl = await this.getOGimage(data.url);
+      createNewsCard(this.container, data, this.defaultImageUrl);
+    }
+    newsTest == 1 ? (this.button.innerHTML = "Altre news") : (this.button.innerHTML = this.testoPulsanteErrore);
+  }
+  async getOGimage(url) {
+    try {
+      let newsMainUrl = new URL(this.proxyUrl + url);
+      let mainUrlResponse = await fetch(newsMainUrl);
+      let data = await mainUrlResponse.text();
+      const parser = new DOMParser();
+      const html = parser.parseFromString(data, "text/html");
+      const imageUrl = html
+        .querySelector('meta[property="og:image"]')
+        .getAttribute("content");
+      return new URL(imageUrl);
+    } catch (e) {
+      console.log("getOGImage Error: " + e);
+      return this.defaultImageUrl;
+    }
+  }
+}
+
+const newsApp = new NewsApp();
+let newsTest = await newsApp.getNewsIds();
+newsTest == 1 ? newsApp.getNewsFromId() : null;
